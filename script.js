ead2f4f98875e083318a44d458e257ea0ebc1107
shakoor94 <shakoor.fida1@gmail.com>
2022-06-27 14:58:40 +0200

    first commit

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("test")
+
+/*
+commentaire :
+Nous allons apprendre
+1. les variables 
+2. les conditions
+3. les boucles
+4.les fonctions
+*/
+
+//les variables
+
+/*
+stoker des données 
+*/
+a=10
+b=50
+c="80"
+b=90
+console.error(a+b)
+console.error(typeof(c))
+
+SEL=100
+SUCRE=80
+
+console.error ("Nous avons du SEL en Q. de " + SEL + ". Nous avons du sucre en quantité de " + SUCRE)
+
+// VOTRE CODE INVERSE LES VARIABLES 
+
+tmp=SEL
+SEL=SUCRE
+SUCRE=tmp
+console.error ("Nous avons du SEL en Q. de " + SEL + ". Nous avons du sucre en quantité de " + SUCRE)
